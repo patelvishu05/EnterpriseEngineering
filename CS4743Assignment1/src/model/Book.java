95a352ed1f0b57e0a2dd87d3ff5be7dce59f21fd
patelvishu05 <35935851+patelvishu05@users.noreply.github.com>
2019-01-17 18:19:14 -0600

    book model

---
+++ CS4743Assignment1/src/model/Book.java
@@ -0,0 +1,55 @@
+package model;
+
+public class Book 
+{
+	private String title;
+	private String author;
+	private String genre;
+	private String ISBN;
+	
+	Book(String t, String a, String g, String i)
+	{
+		this.title = t;
+		this.author = a;
+		this.genre = g;
+		this.ISBN = i;
+	}
+
+	//---------------------ACCESSORS---------------------//
+	
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	
+	
+	
+
+}
