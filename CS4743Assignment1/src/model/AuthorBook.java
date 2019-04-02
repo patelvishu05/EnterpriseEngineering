96a60cce065a91e6e0a50752cd04795afdbcdb1a
Juan Diaz Sada <jdsada30@gmail.com>
2019-04-02 15:35:17 -0500

    AuthorBook model

---
+++ CS4743Assignment1/src/model/AuthorBook.java
@@ -0,0 +1,45 @@
+package model;
+
+
+
+public class AuthorBook {
+	
+	private Author author;
+	private Book book;
+	private int royalty;
+	private boolean newRecord;
+	
+	AuthorBook(){
+		this.author = null; 
+		this.book = null;  
+		this.royalty=0; 
+		this.newRecord = true; 
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	public Book getBook() {
+		return book;
+	}
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	public int getRoyalty() {
+		return royalty;
+	}
+	public void setRoyalty(int royalty) {
+		this.royalty = royalty;
+	}
+	public boolean isNewRecord() {
+		return newRecord;
+	}
+	public void setNewRecord(boolean newRecord) {
+		this.newRecord = newRecord;
+	} 
+	
+	
+
+}
